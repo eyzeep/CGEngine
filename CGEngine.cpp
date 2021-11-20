7946fa65cabb032c4482acb60dd66e1ee60ee1d9
vlados <vlados@NIGGER-KILLA>
2021-11-20 21:59:27 +0300

    Добавьте файлы проекта.

---
+++ CGEngine.cpp
@@ -0,0 +1,15 @@
+﻿// CGEngine.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnCGEngine()
+{
+
+
+
+
+
+}
